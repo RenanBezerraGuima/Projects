b403a71a755d21b4b57351da99943a45d37df82b
Renan Bezerra Guimaraes <renanbezerraguima@proton.me>
2023-06-12 21:37:20 -0300

    Atualizações e criação do ep one line

--- Chaim/atividadeMatriz1.c
+++ Chaim/atividadeMatriz1.c
@@ -4,31 +4,33 @@
 
 int contaCaracteres(char** palavras, int n)
 {
-    int contador = 0;
+    int total = 0;
+
     for (int i=0; i<n ; i++)
     {
         for ( int j = 0; palavras[i][j] != '\0'; j++)
         {
-            contador++;
+            total++;
         }
     }
-    return contador;
+    return total;
 }
 
-int main ()
-{
-    char **matriz = (char**) malloc(sizeof(char*)*3);
+int main() {
+  char** nomes2 = (char**) malloc(sizeof(char*)*4);
+  nomes2[0] = (char*) malloc(sizeof(char)*10);
+  nomes2[1] = (char*) malloc(sizeof(char)*6);
+  nomes2[2] = (char*) malloc(sizeof(char)*6);
+  nomes2[3] = (char*) malloc(sizeof(char)*9);
+  
+  nomes2[0] = "Alvenaria";
+  nomes2[1] = "Vinil";
+  nomes2[2] = "Fibra";
+  nomes2[3] = "Plastico";
 
-    matriz[0] = (char*) malloc(sizeof(char)*7);
-    matriz[1] = (char*) malloc(sizeof(char)*5);
-    matriz[2] = (char*) malloc(sizeof(char)*6);
-
-    matriz[0] = "Tackle";
-    matriz[1] = "Ruck";
-    matriz[2] = "Scrum";
-
-    printf("Minha funcao: %i\n",contaCaracteres(matriz,3));
-    int tamanhoTotal = strlen(matriz[0]) + strlen(matriz[1]) + strlen(matriz[2]);
-    printf("Strlen : %i\n", tamanhoTotal);
-    return 0;
+printf("Total de caracteres (minha funcao): %i\n", contaCaracteres(nomes2,4));
+int tamanhoStrlen = strlen(nomes2[0]) + strlen(nomes2[1]) + strlen(nomes2[2]) + strlen(nomes2[3]);
+printf("Total de caracteres (strlen): %i\n", tamanhoStrlen);
+  
+  return 0;
 }
--- Lista_Geral/raiz.c
+++ Lista_Geral/raiz.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
-double raiz(double a){
+double raiz(double a)
+{
 return sqrt(a);
 }
 
-int main () {
+int main () 
+{
 	double a;
-	printf("Insira o número que deseja saber a raiz: ");
+	printf("Insira o numero que deseja saber a raiz: ");
 	scanf("%lf", &a);
-	printf("A raiz de %.2lf é: %.2lf", a, raiz(a));
+	printf("A raiz de %.2lf eh: %.2lf", a, raiz(a));
 }
