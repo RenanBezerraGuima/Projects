2ea8c896432f9c1e4e2da3175b481f6861ae8206
Renan Bezerra Guimaraes <renanbezerraguima@proton.me>
2023-07-10 10:42:48 -0300

    testes

--- Chaim/testeBuscaBinaria.c
+++ Chaim/testeBuscaBinaria.c
@@ -5,49 +5,43 @@
 
 void algoritmo(int arranjo[], int tam)
 {
-    int inicio = 0;
-    int fim = tam;
-    int meio = (fim + inicio) / 2;
-    
+    printf("Medidas do Arranjo:\n");
+    for (int i=0;i<tam;i++)
+    {
+      printf("%i ", arranjo[i]);
+    }
+    printf("\n\n");
+
     //Média
-    float soma = 0, media;
+    double soma = 0, media;
     for ( int i=0; i< tam; i++)
     {
         soma = arranjo[i] + soma;
     }
     media = soma/tam;
+    
 
     //Mediana
-    int mediana = arranjo[meio];
-
-    printf("%f\n", media);
-    printf("%i\n", meio);
-
-}
-
-int buscaBin(int arr[],int tam, int el)
-{
-  int fim = tam-1;
-  int ini = 0;
-  while (ini <= fim)
-  {
-    int meio = (fim + ini)/2;
-    if (arr[meio] < el)
-    ini = meio + 1;
-    else 
-    {
-        if (arr[meio] > el)
-        fim = meio - 1;
-        else return meio;
-    }
-  }
-  return -1;
+    int meio = (tam+1)/2;
+    double mediana;
+    if ((tam+1)%2 == 0) mediana = arranjo[meio-1];
+      else 
+      {
+        double somaMediana = arranjo[meio] + arranjo[meio-1];
+        mediana = somaMediana/2;
+      }
+    printf("A media eh de %.2lf\n", media);
+    printf("A mediana eh de %.2lf\n", mediana);
+    printf("\n");
 }
 
 int main() {
     int arranjo[] = {1,2,3,4,5,6,7,8,9,10};
+    int arranjo2[] = {1,2,3,4,5,6,7,8,9};
 
+    printf("Calculadora de Media e Mediana\n\n");
     algoritmo(arranjo,10);
+    algoritmo(arranjo2,9);
   
   return 0;
 }
