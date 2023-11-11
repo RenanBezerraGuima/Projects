5bd51af3024f27fe59f5acc6a5d2f20b00660d42
Renan Bezerra Guimaraes <renanbezerraguima@proton.me>
2023-11-11 08:25:53 -0300

    comit

--- AED1/listaLigadaDinamica.c
+++ AED1/listaLigadaDinamica.c
@@ -6,7 +6,7 @@ typedef struct estrutura {
     int chave;
     //infos adicionais
     struct estrutura* prox;
-} NO;
+}NO;
 
 typedef struct {
     NO* inicio;
@@ -51,7 +51,7 @@ bool excluir (lista* l, int ch) {
 }
 
 bool inserir(lista* l, int ch){
-    NO* ant;
+    NO* ant = NULL;
     NO* atual = busca(l, ch, &ant);
     if (atual) return false; //Elemento já existe
     NO* novo = (NO*) malloc(sizeof(NO));
@@ -59,17 +59,95 @@ bool inserir(lista* l, int ch){
     if(ant){ //Elemento no meio
         novo->prox = ant->prox;
         ant->prox = novo;        
-    } else {//Elemento no comço ou lista vazia
+    } else {//Elemento no começo ou lista vazia
         novo->prox = l->inicio;
         l->inicio = novo;
     }
     return true;
 }
 
+bool inserirRepitidas(lista* l, int ch){
+    NO* ant = NULL;
+    NO* p = l->inicio;
 
+    NO* novo = (NO*) malloc (sizeof(NO));
+    novo->chave = ch;
+    novo->prox = NULL;
+
+    if (!p) //lista vazia
+    {
+        l->inicio = novo;
+        return (true);
+    }
+
+    while (p)
+    {
+        ant = p;
+        p = p->prox;
+    }    
+    ant->prox = novo;
+    return (true);
+}
+
+//Dada uma lista ligada dinâmica l não ordenada que admite repetição de chaves, 
+//e um valor de chave x, excluir todas as ocorrências de x na lista.
+void excluirOcorrencias(lista* l, int x){
+    NO* ant = NULL;
+    NO* p = l->inicio;
+
+    while (p)
+    {
+        if (p->chave == x) 
+        {
+            if (ant) ant->prox = p->prox;
+            else l->inicio = p->prox;
+
+            NO* aux = p->prox;
+            free (p);
+            p = aux;
+        }
+        else 
+        {
+            ant = p;
+            p = p->prox;
+        }
+    }
+}
+
+//Dada uma lista ligada dinâmica apontada por *p, crie uma cópia da mesma (usando um malloc para cada elemento 
+//e fazendo as devidas ligações), e retorne um novo ponteiro indicando o começo da lista assim criada.
+NO* copia(NO* p){
+    NO* fim = NULL;
+    NO* inicio = NULL;
+
+    while (p)
+    {
+        NO* novo = (NO*) malloc(sizeof(NO));
+        novo->chave = p->chave;
+        novo->prox = NULL;
+
+        if (!fim) inicio = novo; //Primeiro da lista
+        else fim->prox = novo;
+
+        fim = novo;
+        p = p->prox;
+    }
+    return (inicio);
+}
 
 int main () {
     lista l;
-    
+    inicializa(&l);
+    inserirRepitidas(&l, 10);
+    inserirRepitidas(&l, 20);
+    inserirRepitidas(&l, 30);
+    inserirRepitidas(&l, 40);
+    exibir(&l);
+
+    NO* inicio2 = copia(l.inicio);
+    lista l2;
+    l2.inicio = inicio2;
+    exibir(&l2);
+
     return 0;
 }
