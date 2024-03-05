29b8f66eb6210e122762bfd47b8f439c19bd1b73
Renan Windows <renanbezerraguima@proton.me>
2024-03-05 19:58:12 -0300

    organizing

---
+++ 3Semestre/AED2/grafo_matrizadj.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "grafo_matrizadj.h"
+
+/*
+    InicializaGrafo(Grafo* grafo, int nv): Inicializa um grafo com nv vertices
+    Vertices vao de 1 a nv
+    Preenche as celulas com AN (representando ausencia de aresta)
+    Retorna true se inicializou com sucesso e false caso contrario
+*/
+bool inicializaGrafo(Grafo *grafo, int nv)
+{
+    int i, j;
+
+    if (nv > MAXNUMVERTICES)
+    {
+        fprintf(stderr, "ERRO na chamada de inicializaGrafo: Numero de vertices maior que o o maximo perimitido de %d.\n", MAXNUMVERTICES);
+        return false;
+    }
+
+    if (nv <= 0)
+    {
+        fprintf(stderr, "ERRO na chamada de inicializaGrafo: Numero de vertices deve ser positivo.\n");
+        return false;
+    }
+
+    grafo->numVertices = nv;
+    grafo->numArestas = 0;
+    for (i = 0; i < grafo->numVertices; i++)
+    {
+        for (j = 0; j < grafo->numVertices; j++)
+        {
+            grafo->mat[i][j] = AN;
+        }
+    }
+    return true;
+}
---
+++ 3Semestre/AED2/grafo_matrizadj.h
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+
+#define MAXNUMVERTICES 100
+#define AN -1
+
+typedef int Peso;
+typedef struct
+{
+    Peso mat[MAXNUMVERTICES][MAXNUMVERTICES];
+    int numVertices;
+    int numArestas;
+} Grafo;
