4858dddda3290c78759286ed977eed70041e6e1b
Renan Bezerra Guima <renanbezerraguima@proton.me>
2024-03-08 09:19:32 -0300

    grafos

--- 3Semestre/AED2/Grafo_Direcionado/testa_grafo_matrizadj.c
+++ 3Semestre/AED2/Grafo_Direcionado/testa_grafo_matrizadj.c
@@ -16,6 +16,12 @@ int main()
     imprimeGrafo(&g1);
     printf("%d\n", existeAresta(2, 5, &g1));
     printf("%d\n", existeAresta(0, 1, &g1));
+    printf("%d\n", obtemPesoAresta(2, 5, &g1));
+    printf("%d\n", obtemPesoAresta(0, 1, &g1));
+    printf("%d\n", proxListaAdj(2, &g1, -1));
+
+    printf("%d\n", listaAdjVazia(2, &g1));
+    printf("%d\n", listaAdjVazia(0, &g1));
 
     return 0;
 }
